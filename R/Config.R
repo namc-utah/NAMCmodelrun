181016ca17d163647b9fd3f909b91505f456fbde
courtwrightj01 <jennifer.courtwright@usu.edu>
2022-03-03 15:29:37 -0600

    adding code for CO model inputs and outputs

--- R/Config.R
+++ R/Config.R
@@ -21,9 +21,16 @@ library(nhdplusTools)
 library(devtools)
 library(BMIMetrics)
 library(CSCI)
-boxId=2141# 2141-UT,2065 OR WCCP and MCCP, null, 2152 PIBO, 2172 CSCI,2107 AREMP, 2054 CO, NV 2140
+library("factoextra")
+boxId=2141# 2141-UT,2065 OR WCCP and MCCP, null, 2152 PIBO, 2172 CSCI,2107 AREMP, 2054 CO, NV 2140, 1603 westwide and 2055,	2150 WY
+projects=NAMCr::query("projects")
+#projectId=
 models=NAMCr::query("models")
-modelId=2
+modelId=8
+modelId=3
+#prednew=read.csv("C:/Users/jenni/Box/NAMC (Trip Armstrong)/OE_Modeling/NAMC_Supported_OEmodels/Wyoming/InputsAndResults/CurrentRun/WY_Habitat.csv", row.names="SampleID")
+#prednew=read.csv("C:/Users/jenni/Box/NAMC (Trip Armstrong)/OE_Modeling/NAMC_Supported_OEmodels/West_Wide_Model/NewWestWIde_NONMidgeModel/InputsAndResults/AIM_NM_MT_2019/WW18_Preds.csv", row.names="SampleID")
+#prednew=read.csv("C:/Users/jenni/Box/NAMC (Trip Armstrong)/OE_Modeling/NAMC_Supported_OEmodels/West_Wide_Model/NewWestWIde_NONMidgeModel/InputsAndResults/CurrentRun/WW18_Preds.csv", row.names="SampleID")
 #prednew=read.csv("C:/Users/jenni/Box/NAMC (Trip Armstrong)/OE_Modeling/NAMC_Supported_OEmodels/AREMP2014/InputsAndResults/2020/Test_preds.csv",row.names="SampleID")
 #prednew=read.csv("C:/Users/jenni/Box/NAMC (Trip Armstrong)/OE_Modeling/NAMC_Supported_OEmodels/CA/Hybrid_CA_Model/InputsAndResults/CurrentRun/habitat.csv")
 #prednew=read.csv("C:/Users/jenni/Box/NAMC (Trip Armstrong)/OE_Modeling/NAMC_Supported_OEmodels/PIBO/InputsAndResults_PIBO2009oe/AIM_ID_2020/Habitat.csv",row.names = "SAMPLEID")
@@ -34,7 +41,14 @@ prednew=read.csv("C:/Users/jenni/Box/NAMC (Trip Armstrong)/OE_Modeling/NAMC_Supp
 SQLite_file_path="C:/NAMC_S3/StreamCat/StreamCat2022.sqlite"
 CalPredsModelApplicability=read.csv("C:/Users/jenni/Box/NAMC (Trip Armstrong)/OE_Modeling/NAMC_Supported_OEModels/Model Applicability/CalPredsModelApplicability.csv")
 
+
+
+
+#29-37
+#254-end
+
 source("R/Bug_functions_box.R")
+source("R/Bug_functions_sample.R")
 source("R/Model_functions.R")
 source("R/model.predict.RanFor.4.2.r")
 source("R/model.predict.v4.1.r")
