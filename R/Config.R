abc0eea323fd5f0cf85eb6f70f20d1ce8f89ee89
courtwrightj01 <jennifer.courtwright@usu.edu>
2022-03-22 09:34:25 -0500

    additional clean up of config file

--- R/Config.R
+++ R/Config.R
@@ -16,16 +16,20 @@ source("R/model.predict.v4.1.r")
 source("R/ModelApplicabilityAll.R")
 
 
+# to run the scripts choose a boxId or projectId
 boxId=2141
 # test boxes
 # 2141-UT,2065 OR WCCP and MCCP, null, 2152 PIBO, 2172 CSCI,2107 AREMP, 2054 CO, NV 2140, 1603 westwide and 2055,	2150 WY
 
 projects=NAMCr::query("projects")
 #projectId=
-models=NAMCr::query("models")
 
+
+# then input a modelID
+models=NAMCr::query("models")
 modelID=3
 
+# input file path for reference sites attributed with elevation, watershed area, and temperature for model applicability function
 #remove this line once reference sites are all in database with stream cat data
 CalPredsModelApplicability=read.csv("C:/Users/jenni/Box/NAMC (Trip Armstrong)/OE_Modeling/NAMC_Supported_OEModels/Model Applicability/CalPredsModelApplicability.csv")
 
