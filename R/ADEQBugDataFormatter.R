f0355c051df4da300d67d694859feb110dc6c3a8
Andrew Caudillo <andrew.caudillo@usu.edu>
2023-08-31 09:41:42 -0600

    updating to include lifestage (required for ADEQ input)

--- R/ADEQBugDataFormatter.R
+++ R/ADEQBugDataFormatter.R
@@ -1,3 +1,4 @@
+
 ADEQ_bug_export<-function(sampleIds){
   bugRaw = NAMCr::query(
     "sampleTaxaUnambiguous",
@@ -14,7 +15,7 @@ ADEQ_bug_export<-function(sampleIds){
   )
   samples = NAMCr::query(
     "samples",
-    include = c("boxId","sampleId",'siteId','sampleDate',"siteName", "waterbodyName","sampleMethod","habitatName","area", "labSplit"),# possibly add waterbody name to the samples NAMCr::query
+    include = c("boxId","sampleId",'siteId','sampleDate',"siteName", "waterbodyName","sampleMethod","habitatName","area", "labSplit"),
     sampleIds=sampleIds
   )
 
@@ -22,24 +23,34 @@ ADEQ_bug_export<-function(sampleIds){
   AZbugs=dplyr::left_join(AZsubsamp,bugsTranslation, by=c("taxonomyId", "sampleId"))
   AZbugs=dplyr::left_join(AZsubsamp,samples, by='sampleId')
 
-  AZbugs$StationID=AZbugs$siteName
-  AZbugs$WaterbodyName=AZbugs$waterbodyName
-  AZbugs$ActivityID=AZbugs$sampleId
-  AZbugs$RepNum=1
-  AZbugs$CommentsSample=NA
-  AZbugs$CollDate=AZbugs$sampleDate
-  AZbugs$CollMeth='ADEQ Riffle bugs'
-  AZbugs$CorrectionFactor=100*(1/AZbugs$labSplit)
-  AZbugs$FinalID=AZbugs$scientificName
-  AZbugs$Individuals=AZbugs$splitCount
-  AZbugs$Stage=AZbugs$lifeStageAbbreviation
-  AZbugs$LargeRare='No'
-  AZbugs$Habitat=ifelse(AZbugs$habitatName=='Targeted Riffle','Riffle','Multi-Habitat')
-  AZbugs$Lab='NAMC'
-  AZbugs$LabID=NA
+  sampletax = NAMCr::query(
+    "sampleTaxa",
+    sampleIds=sampleIds
+  )
+  sampletax<-sampletax[!duplicated(sampletax$scientificName),]
+  AZbugs<-merge(AZbugs,sampletax[,c('scientificName','lifeStageAbbreviation')],'scientificName')
+  AZbugs2<-data.frame(matrix(nrow=nrow(AZbugs)));names(AZbugs2)<-'StationID'
+  AZbugs2$StationID=AZbugs$siteName
+  AZbugs2$WaterbodyName=AZbugs$waterbodyName
+  AZbugs2$ActivityID=AZbugs$sampleId
+  AZbugs2$RepNum=1
+  AZbugs2$CommentsSample=NA
+  AZbugs2$CollDate=AZbugs$sampleDate
+  AZbugs2$CollMeth=rep('ADEQ Riffle bugs',nrow(AZbugs))
+  #correction factor is 100 * 1/labsplit %. So we need to force decimal to %.
+  AZbugs2$CorrectionFactor=100*(1/(AZbugs$labSplit*100))
+  AZbugs2$FinalID=AZbugs$scientificName
+  AZbugs2$Individuals=AZbugs$splitCount
+  AZbugs2$Stage=AZbugs$lifeStageAbbreviation
+  AZbugs2$LargeRare='No'
+  AZbugs2$Habitat=ifelse(AZbugs$habitatName=='Targeted Riffle','Riffle','Multi-Habitat')
+  AZbugs2$Lab='NAMC'
+  AZbugs2$LabID=NA
 
-  AZbugs2=AZbugs[,c("StationID","waterbodyName","ActivityID","RepNum","CollDate","CommentsSample","CollMeth",
-                    "CorrectionFactor","FinalID","Individuals","Stage","LargeRare","Habitat","Lab","LabID")]
+
+
+
+
   #write excel file to workspace
   write.csv(AZbugs2,file = paste0("AZbugs","boxId_",boxId,"_",Sys.Date(),".csv"),row.names=FALSE)
   cat(paste("csv with AZbugs has been written out to your current working directory.",
