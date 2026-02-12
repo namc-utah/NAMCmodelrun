db34a59006c073e8d8f41f80aee3e7551a0b98b4
Jennifer Courtwright <a01964518@aggies.usu.edu>
2026-02-12 10:06:00 -0600

    adding files and importing WQ data

--- R/import wq results.R
+++ R/import wq results.R
@@ -1,3 +1,19 @@
+WQ=read.csv("C:/Users/A01964518/Box/NAMC/Research Projects/AIM/WQ data/WQ_Bug_Model_GIS_stats_and_results/2025_predictions/2025_predictedWQimport.csv")
+t=tidyr::pivot_longer(WQ,cols=c(EC_predict,	EC_ModelApplicability,	TN_predict,	TN_ModelApplicability	,TP_predict	,TP_ModelApplicability,	TN_Observed	,TP_Observed,	EC_Observed
+))
+library(tidyverse)
+df_separated_delim <- t %>%
+  separate_wider_delim(
+    cols = name, # Column to split
+    delim = "_",     # Delimiter character (a space)
+    names = c("Model", "param") # Names for the new columns
+  )
+write.csv(df_separated_delim,'wq.csv')
+WQ=read.csv('wq.csv')
+finalResults=tidyr::pivot_wider(WQ,names_from=param,values_from=value)
+finalResults=na.omit(finalResults)
+finalResults$modelResult=finalResults$oResult-finalResults$eResult
+
 finalResults=read.csv("C:/Users/A01964518/Box/NAMC/OEModeling/NAMC_Supported_OEmodels/WaterQuality/Updated WQ Models/import into database.csv")
 for (i in 1:nrow(finalResults)){
 
@@ -6,8 +22,8 @@ for (i in 1:nrow(finalResults)){
   api_endpoint = "setModelResult",
   sampleId = finalResults$sampleId[i],
   modelId = finalResults$modelId[i],
-  oResult=finalResults$o_result[i],
-  eResult=finalResults$e_result[i],
+  oResult=finalResults$oResult[i],
+  eResult=finalResults$eResult[i],
   modelResult = finalResults$modelResult[i],
   modelApplicability = finalResults$ModelApplicability[i],
   fixedCount=0
